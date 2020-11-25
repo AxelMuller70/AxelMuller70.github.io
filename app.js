95de1ce9d55cf9123d9c9b91cb27e106122cd1c7
AxelMuller70 <axel.muller70@gmail.com>
2020-11-25 15:21:28 +0100

    ye

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 /************************************************************************
  *                                                                      *
  *                  Ginko Bus Progressive Web Application               *
@@ -7,7 +9,10 @@
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
 
-    
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js');
+    };
+
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
     ******************************************************************/
